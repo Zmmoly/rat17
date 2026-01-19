cffa47ff9a208af364552209e7ec3fc5f8fed501
File Explorer App <app@example.com>
2026-01-19 12:08:20 +0200

    تحميل tmp_src/org/gradle/wrapper/GradleWrapperMain.java

---
+++ tmp_src/org/gradle/wrapper/GradleWrapperMain.java
@@ -0,0 +1,8 @@
+package org.gradle.wrapper;
+public class GradleWrapperMain {
+    public static void main(String[] args) {
+        System.err.println("ERROR: This is a placeholder wrapper.");
+        System.err.println("Please use 'gradle wrapper' to generate the proper wrapper files.");
+        System.exit(1);
+    }
+}
